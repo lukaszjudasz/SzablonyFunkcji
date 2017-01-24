7ac6c2f74e12134a96787a34e2844b4be55c768b
LukaszJudasz <lukasz19911@gmail.com>
2017-01-24 22:42:01 +0100

    max template

---
+++ SzablonyFunkcji/SzablonyFunkcji.cpp
@@ -0,0 +1,29 @@
+// SzablonyFunkcji.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+template <class T>
+T pobierz_max(T a, T b)
+{
+	T wynik;
+	wynik = (a > b) ? a : b;
+	return wynik;
+}
+
+
+int main()
+{
+	int max_int;
+	double max_double;
+
+	max_int = pobierz_max(-7, -7);
+	max_double = pobierz_max(52.10, -5414.36);
+
+	cout << "int_max " << max_int<<endl;
+	cout << "int_double " << max_double << endl;
+
+	return 0;
+}
