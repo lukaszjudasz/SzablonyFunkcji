59b0f463daac0995166bf6777618b6b719f2d2a4
LukaszJudasz <lukasz19911@gmail.com>
2017-01-25 21:53:50 +0100

    Run correctly

--- SzablonyFunkcji/SzablonyFunkcji.cpp
+++ SzablonyFunkcji/SzablonyFunkcji.cpp
@@ -1,6 +1,3 @@
-// SzablonyFunkcji.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
@@ -10,26 +7,43 @@ T sortuj(T tab[], int ilosc, bool czy_rosnaco)
 {
 	T wynik;
 	wynik = tab[0];
-	for (int i = 1; i < ilosc; i++)
+	if (czy_rosnaco)
 	{
-		if (tab[i] > wynik)
+		for (int i = 1; i < ilosc; i++)
 		{
-			wynik = tab[i];
+			if (tab[i] > wynik)
+			{
+				wynik = tab[i];
+			}
 		}
-		return wynik;
 	}
+	else
+	{
+		for (int i = 1; i < ilosc; i++)
+		{
+			if (tab[i] < wynik)
+			{
+				wynik = tab[i];
+			}
+		}
+	}
+	return wynik;
 }
-
-
 int main()
 {
-	int tab_int[] = {1, 2, 8, 4, 5};
-	double tab_double[] = {2.4, 2.7, 7.3, 1.8, 9.9};
+	int tab_int[] = {1, 7, 8, 0, -9};
+	double tab_double[] = {2.4, 2.7, -7.3, 1.8, 9.9};
 	int tab_int_size = sizeof(tab_int) / sizeof(tab_int[0]);
 	int tab_double_size = sizeof(tab_double) / sizeof(tab_double[0]);
-	//cout << tab_int_size << endl;
-	//cout << tab_double_size << endl;
-	double wynik = sortuj(tab_int, tab_int_size, true);
-	cout << wynik;
+
+	double min_int = sortuj(tab_int, tab_int_size, false);
+	double max_int = sortuj(tab_int, tab_int_size, true);
+	double min_double = sortuj(tab_double, tab_double_size, false);
+	double max_double = sortuj(tab_double, tab_double_size, true);
+
+	cout << "Minimalna wartosc z tablicy int[] wynosi: " << min_int << endl;
+	cout << "Maksymalna wartosc z tablicy int[] wynosi: " << max_int << endl;
+	cout << "Minimalna wartosc z tablicy double[] wynosi: " << min_double << endl;
+	cout << "Maksymalna wartosc z tablicy double[] wynosi: " << max_double << endl;
 	return 0;
 }
