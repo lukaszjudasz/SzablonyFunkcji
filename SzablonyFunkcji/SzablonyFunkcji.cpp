35d5464ff914ca6116d0aadce2cde3720c64aebf
LukaszJudasz <lukasz19911@gmail.com>
2017-01-25 21:33:25 +0100

    Problem with for loop

--- SzablonyFunkcji/SzablonyFunkcji.cpp
+++ SzablonyFunkcji/SzablonyFunkcji.cpp
@@ -1,4 +1,4 @@
-// SzablonyFunkcji.cpp : Defines the entry point for the console application. TEST
+// SzablonyFunkcji.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -6,24 +6,30 @@
 using namespace std;
 
 template <class T>
-T pobierz_max(T a, T b)
+T sortuj(T tab[], int ilosc, bool czy_rosnaco)
 {
 	T wynik;
-	wynik = (a > b) ? a : b;
-	return wynik;
+	wynik = tab[0];
+	for (int i = 1; i < ilosc; i++)
+	{
+		if (tab[i] > wynik)
+		{
+			wynik = tab[i];
+		}
+		return wynik;
+	}
 }
 
 
 int main()
 {
-	int max_int;
-	double max_double;
-
-	max_int = pobierz_max(-7, -7);
-	max_double = pobierz_max(52.10, -5414.36);
-
-	cout << "int_max " << max_int<<endl;
-	cout << "int_double " << max_double << endl;
-
+	int tab_int[] = {1, 2, 8, 4, 5};
+	double tab_double[] = {2.4, 2.7, 7.3, 1.8, 9.9};
+	int tab_int_size = sizeof(tab_int) / sizeof(tab_int[0]);
+	int tab_double_size = sizeof(tab_double) / sizeof(tab_double[0]);
+	//cout << tab_int_size << endl;
+	//cout << tab_double_size << endl;
+	double wynik = sortuj(tab_int, tab_int_size, true);
+	cout << wynik;
 	return 0;
 }
